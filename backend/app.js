b31c32c54cc95662d680cb78c361ad791e23fcbb
maya-memon <marriamnoor68@gmail.com>
2025-08-08 09:38:06 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+  origin: ["https://hospital-user-frontend.netlify.app/", process.env.DASHBOARD_URL],
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
 }));
