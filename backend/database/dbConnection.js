242a085b9605b798edc500cdc4f11b86c4a3ef61
maya-memon <marriamnoor68@gmail.com>
2025-08-09 11:02:00 +0500

    chnages in db conection

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-export const  dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName:"MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM"
-    }).then(()=>{
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
         console.log("connected succesfuully");
 
-    }).catch(err=>{
+    }).catch(err => {
         console.log(`Some error occuured while connecting database ${err}`)
     })
 }
